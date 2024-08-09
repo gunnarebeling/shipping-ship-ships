ff4e7f2964409201718f1ce1eb24d3b6820e5c59
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-09 10:48:28 -0500

    changes

--- scripts/database.js
+++ scripts/database.js
@@ -1,15 +1,15 @@
 const database = {
     docks: [
-        { id: 1, location: "Shanghai, China", volume: "43.5" },
-        { id: 2, location: "Busan, South Korea", volume: "21.6" },
-        { id: 3, location: "Rotterdam, The Netherlands", volume: "14.35" },
-        { id: 4, location: "Antwerp, Belgium", volume: "12.04" }
+        { id: 1, location: "Shanghai, China", volume: "43.5", HaulerId: [1,2]},
+        { id: 2, location: "Busan, South Korea", volume: "21.6", haulerId:[1,2,3,4] },
+        { id: 3, location: "Rotterdam, The Netherlands", volume: "14.35", haulerId: [4] },
+        { id: 4, location: "Antwerp, Belgium", volume: "12.04", haulerId:[] }
     ],
     haulers:[
-        {id: 1, name: "Big Boy", dockId: 2 },
-        {id: 2, name: "The Taj Ma Haul", dockId: 2},
-        {id: 3, name:"Haul n Boats", dockId: 4},
-        {id: 4, name: "Old Hauler", dockId: 2}
+        {id: 1, name: "Big Boy"  },
+        {id: 2, name: "The Taj Ma Haul", },
+        {id: 3, name:"Haul n Boats"},
+        {id: 4, name: "Old Hauler"}
     ],
     shippingShips: [
         {id: 1, name: "Little Guy", haulerId: 1},
@@ -24,11 +24,14 @@ const database = {
 }
 
 export const  getDocks = () =>{
-    return database.docks.map(docks => ({...docks}))
+    return database.docks.map(docks => ({...docks})).sort((a, b)=>
+        a.location.localeCompare(b.location))
 }
 export const getHaulers =() => {
-    return database.haulers.map(hauler => ({...hauler}))
+    return database.haulers.map(hauler => ({...hauler})).sort((a, b)=>
+        a.name.localeCompare(b.name))
 }
 export const getShippingShips = () => {
-    return database.shippingShips.map(ships => ({...ships}))
+    return database.shippingShips.map(ships => ({...ships})).sort((a, b)=>
+        a.name.localeCompare(b.name))
 }
--- scripts/dockList.js
+++ scripts/dockList.js
@@ -1,19 +1,20 @@
 import { getDocks } from "./database.js";
 import { getHaulers } from "./database.js";
+const docks = getDocks()
 
+const haulerList = getHaulers();
 
 
 
 
 export const dockList = () => {
-    const docks = getDocks()
 
     let docksHTML = "<ul class='docks'>"
     docksHTML += `<h1>Docks</h1>`
 
     for (const dock of docks) {
     
-        docksHTML += `<li data-type="dock" data-id="${dock.id}" data-volume="${dock.volume}" data-location="${dock.location}">${dock.location} can hold ${dock.volume} tons of cargo</li>`
+        docksHTML += `<li data-type="dock" data-id="${dock.id}" data-volume="${dock.volume}" data-location="${dock.location}" data-haulerid="${dock.haulerId.join(",")}>${dock.location} can hold ${dock.volume} tons of cargo</li>`
     }
 
     docksHTML += "</ul>"
@@ -24,18 +25,20 @@ document.addEventListener(
     "click",
     (clickEvent) => {
         const itemClicked = clickEvent.target
-        const haulerList = getHaulers();
         const dockId = parseInt(itemClicked.dataset.id)
         
         if (itemClicked.dataset.type === "dock") {
-            let atDockHaulers = haulerList.filter(hauler => hauler.dockId === dockId);
-            let haulerListNames = atDockHaulers.map(haulers => haulers.name)
-            haulerListNames = haulerListNames.join(', ');
-            if (atDockHaulers.length === 0) {
-                haulerListNames = "nothing"
-            } 
+        //     let atDockHaulers = haulerList.filter(hauler => hauler.dockId === dockId);
+        //     let haulerListNames = atDockHaulers.map(haulers => haulers.name)
+        //     haulerListNames = haulerListNames.join(', ');
+        //     if (atDockHaulers.length === 0) {
+        //         haulerListNames = "nothing"
+        //     } 
                 
-                window.alert(`${itemClicked.dataset.location} is currently unloading ${haulerListNames} `)
+        //         window.alert(`${itemClicked.dataset.location} is currently unloading ${haulerListNames} `)
+            haulerList.forEach(hauler => {
+                
+            });
             
         }
 
